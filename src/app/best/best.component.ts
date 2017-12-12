a06dfa9bdb1842f798e8eb134f5ef915af23317e
snolly <kassnollygv4462>
2017-12-12 20:41:48 +0300

    add info win

--- src/app/best/best.component.ts
+++ src/app/best/best.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, NgZone } from '@angular/core';
 import { MainService} from '../core/services/main.service';
 import { AgmCoreModule } from '@agm/core';
 
@@ -12,8 +12,22 @@ export class BestComponent implements OnInit {
   constructor(private service:MainService) { }
   lat: number = 51.678418;
   lng: number = 7.809007;
-    ngOnInit() {
+  isInfoWinOpen:boolean[] = [];
+
+     ngOnInit() {
       this.service.onPageChange$.next(false);
+      this.clearInfoWin();
     }
 
+    clearInfoWin(){
+      this.isInfoWinOpen = [];
+      for(let i=0;i<2;i++)this.isInfoWinOpen.push(false);
+    }
+    mapClick(){
+      this.clearInfoWin();
+    }
+    markerClick(i:number){
+      this.isInfoWinOpen[i]= !this.isInfoWinOpen[i];
+    }
+   
 }
