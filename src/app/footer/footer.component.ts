5b9a04305fffcd2d69031b3348a3bf84f3e6506d
lexa <keitmark@mail.ru>
2017-12-14 00:27:24 +0300

    ll

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MainService} from '../core/services/main.service';
+import {NgForm} from '@angular/forms';
+import { Router } from "@angular/router";
+
 
 @Component({
   selector: 'app-footer',
@@ -10,6 +14,10 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  @ViewChild('submitFormCwrc') form: NgForm
+  SubscribeEmail(){
+    console.log(this.form);
   }
 
 }
