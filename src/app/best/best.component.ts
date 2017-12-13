b410594a2b0a7ab60af54c93050127935e405044
lexa <keitmark@mail.ru>
2017-12-13 16:06:55 +0300

    lala

--- src/app/best/best.component.ts
+++ src/app/best/best.component.ts
@@ -17,6 +17,8 @@ export class BestComponent implements OnInit {
   lng: number = 7.809007;
   MapStyle = this.getMapStyle();
   flagForOpenSlider:boolean = true;
+  newFlagForVisible:boolean = false;
+
 
   Params = {
     limit:10,
@@ -95,6 +97,7 @@ export class BestComponent implements OnInit {
  
 
     OpenSliderCart(index:number){
+      this.newFlagForVisible = false;
       this.allSightByRoute = this.allBestRouts[index].places;
       console.log(this.allSightByRoute);
 
@@ -104,15 +107,18 @@ export class BestComponent implements OnInit {
       
       this.flagForOpenSlider = false;
      
-      setTimeout(function(){
-          $('.flex-sights').slick({
+      setTimeout(()=>{
+
+        this.newFlagForVisible = true;
+
+        $('.flex-sights').slick({
             slidesToShow: 6,
             slidesToScroll: 1,
             arrows: true,
             dots: false,
             infinite:false
         });
-      },500);
+      },200);
       
     
       
@@ -132,8 +138,9 @@ export class BestComponent implements OnInit {
       
     }
 
-    OpenModalSights(){
+    OpenModalSights(index){
         $("#sights-slider").modal("show");
+        console.log(index);
         $('.slider-init').slick({
             slidesToShow: 1,
             slidesToScroll: 1,
@@ -141,6 +148,7 @@ export class BestComponent implements OnInit {
             dots: false,
             infinite:false
         });
+        $('.slider-init').slick('slickGoTo',index,true);
     }
 
     getMapStyle(){
